e80c5fc03464be28c8c1f32e8994917fdf43012a
余嘉舜 <56122682+Shun05323110487107213062@users.noreply.github.com>
2022-01-20 11:58:59 +0800

    Update check.py

--- check.py
+++ check.py
@@ -1,15 +1,15 @@
 import tensorflow as tf
-interpreter = tf.lite.Interpreter(model_path='sps.tflite')
+interpreter = tf.lite.Interpreter(model_path='放入自己訓練模型的路徑')# example：(model_path='sps.tflite')
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 print("== Input details ==")
-print("name:", input_details[0]['name'])
-print("shape:", input_details[0]['shape'])
-print("type:", input_details[0]['dtype'])
+print("name:", input_details[0]['name'])# 物件名字
+print("shape:", input_details[0]['shape'])# [  1 224 224   3] 一個輸出結果；圖片長度壓縮像素；圖片寬度壓縮像素；X個種類的分類
+print("type:", input_details[0]['dtype'])# <class 'numpy.uint8'> 8-bit unsigned integer (0 to 255)
 
 print("\n== Output details ==")
-print("name:", output_details[0]['name'])
-print("shape:", output_details[0]['shape'])
-print("type:", output_details[0]['dtype'])
+print("name:", output_details[0]['name'])# 物件名字
+print("shape:", output_details[0]['shape'])# [1 3] 一個輸出結果；X個種類的分類
+print("type:", output_details[0]['dtype'])# <class 'numpy.uint8'> 8-bit unsigned integer (0 to 255)

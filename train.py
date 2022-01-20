f1be09006d9356652b52cb19c1fb16e8cda2233a
余嘉舜 <56122682+Shun05323110487107213062@users.noreply.github.com>
2022-01-20 12:26:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#此訓練模型不是在Raspberry Pi上面，而是在Windows作業系統上
 import os
 import numpy as np
 import tensorflow as tf
@@ -8,10 +9,10 @@ from tflite_model_maker.config import ExportFormat
 from tflite_model_maker.config import QuantizationConfig
 from tflite_model_maker.image_classifier import DataLoader
 
-image_path = 'C:/Users/User/Desktop/Model/sps'
+image_path = '放入已經分類號類別的圖鑒總檔案的路徑'# example：image_path = 'C:/Users/User/Desktop/Model/sps'
 data = DataLoader.from_folder(image_path)
-train_data, rest_data = data.split(0.8)#訓練集
-validation_data, test_data = rest_data.split(0.5)#把分割0.2再分攤一半
-model = image_classifier.create(train_data, validation_data=validation_data, epochs = 15)
-loss, accuracy = model.evaluate(test_data)#偏差，準度
-model.export(export_dir='.',tflite_filename='sps2.tflite')
+train_data, rest_data = data.split(0.8)# 作爲訓練集
+validation_data, test_data = rest_data.split(0.5)# 再把剩餘的分割為驗證集與測試集
+model = image_classifier.create(train_data, validation_data=validation_data, epochs = 15)# epochs 訓練組的次數（自訂）
+loss, accuracy = model.evaluate(test_data)# 偏差，準度
+model.export(export_dir='.',tflite_filename='輸出檔案的名稱.tflite') # example：tflite_filename='sps.tflite'
